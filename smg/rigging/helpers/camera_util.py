e559c3ea4bf724946c58f9fc40c39631f712540f
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-16 20:42:11 +0000

    Rename some functions.

--- smg/rigging/helpers/camera_util.py
+++ smg/rigging/helpers/camera_util.py
@@ -11,52 +11,52 @@ class CameraUtil:
     # PUBLIC STATIC METHODS
 
     @staticmethod
-    def compute_baseline_c(cam1: Camera, cam2: Camera) -> float:
+    def compute_look_rotation_c(cam1: Camera, cam2: Camera) -> float:
         """
-        Compute the baseline between the two specified cameras (i.e. the distance between their positions).
+        Compute the rotation (in degrees) between the look vectors of the two specified cameras.
 
         :param cam1:    The first camera.
         :param cam2:    The second camera.
-        :return:        The baseline between the two cameras.
-        """
-        return np.linalg.norm(cam1.p() - cam2.p())
-
-    @staticmethod
-    def compute_baseline_p(pose1: np.ndarray, pose2: np.ndarray) -> float:
-        """
-        Compute the baseline between the two specified camera poses (i.e. the distance between their positions).
-
-        :param pose1:   The first pose.
-        :param pose2:   The second pose.
-        :return:        The baseline between the two camera poses.
-        """
-        return CameraUtil.compute_baseline_c(
-            CameraPoseConverter.pose_to_camera(pose1),
-            CameraPoseConverter.pose_to_camera(pose2)
-        )
-
-    @staticmethod
-    def compute_look_angle_c(cam1: Camera, cam2: Camera) -> float:
-        """
-        Compute the angle (in degrees) between the look vectors of the two specified cameras.
-
-        :param cam1:    The first camera.
-        :param cam2:    The second camera.
-        :return:        The angle between the look vectors of the two cameras.
+        :return:        The rotation between the look vectors of the two cameras.
         """
         d: float = np.clip(np.dot(cam1.n(), cam2.n()), -1.0, 1.0)
         return np.rad2deg(np.arccos(d))
 
     @staticmethod
-    def compute_look_angle_p(pose1: np.ndarray, pose2: np.ndarray) -> float:
+    def compute_look_rotation_p(pose1: np.ndarray, pose2: np.ndarray) -> float:
         """
-        Compute the angle (in degrees) between the look vectors of the two specified camera poses.
+        Compute the rotation (in degrees) between the look vectors of the two specified camera poses.
 
         :param pose1:   The first pose.
         :param pose2:   The second pose.
-        :return:        The angle between the look vectors of the two camera poses.
+        :return:        The rotation between the look vectors of the two camera poses.
         """
-        return CameraUtil.compute_look_angle_c(
+        return CameraUtil.compute_look_rotation_c(
+            CameraPoseConverter.pose_to_camera(pose1),
+            CameraPoseConverter.pose_to_camera(pose2)
+        )
+
+    @staticmethod
+    def compute_translation_c(cam1: Camera, cam2: Camera) -> float:
+        """
+        Compute the translation between the two specified cameras (i.e. the distance between their positions).
+
+        :param cam1:    The first camera.
+        :param cam2:    The second camera.
+        :return:        The translation between the two cameras.
+        """
+        return np.linalg.norm(cam1.p() - cam2.p())
+
+    @staticmethod
+    def compute_translation_p(pose1: np.ndarray, pose2: np.ndarray) -> float:
+        """
+        Compute the translation between the two specified camera poses (i.e. the distance between their positions).
+
+        :param pose1:   The first pose.
+        :param pose2:   The second pose.
+        :return:        The translation between the two camera poses.
+        """
+        return CameraUtil.compute_translation_c(
             CameraPoseConverter.pose_to_camera(pose1),
             CameraPoseConverter.pose_to_camera(pose2)
         )
