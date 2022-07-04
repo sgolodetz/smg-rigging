0309a3f55e6c2d2fee8101b37c6ba01ebfc548c8
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-07-04 13:50:14 +0100

    Continue working on README files.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="smg-rigging",
@@ -9,7 +9,7 @@ setup(
     author="Stuart Golodetz",
     author_email="stuart.golodetz@cs.ox.ac.uk",
     description="A simple 3D camera library",
-    long_description="",  #long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sgolodetz/smg-rigging",
     packages=find_packages(include=["smg.rigging", "smg.rigging.*"]),
